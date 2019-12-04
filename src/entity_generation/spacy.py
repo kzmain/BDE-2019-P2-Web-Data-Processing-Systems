705b819b336916465f8f73d01180b5c2268aac0c
Stan Swanborn <stanswanborn@gmail.com>
2019-12-04 13:57:04 +0100

    Minor Spacy change for optimizing entity detection

--- src/entity_generation/spacy.py
+++ src/entity_generation/spacy.py
@@ -3,13 +3,18 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 def generate_entities(payload):
-    document = nlp(payload)
+    doc = nlp(payload)
 
-    for x in document.sents:
-        [(x.orth_,x.pos_, x.lemma_) for x in [y 
-                                      for y
-                                      in nlp(str(x)) 
-                                      if not y.is_stop and y.pos_ != 'PUNCT']]
-        z = dict([(str(x), x.label_) for x in nlp(str(x)).ents])
-        for text, label in z.items():
-            yield (text,label)
+    for ent in doc.ents:
+        yield(ent.text, ent.label_)
+
+    # document = nlp(payload)
+
+    # for x in document.sents:
+    #     [(x.orth_,x.pos_, x.lemma_) for x in [y 
+    #                                   for y
+    #                                   in nlp(str(x)) 
+    #                                   if not y.is_stop and y.pos_ != 'PUNCT']]
+    #     z = dict([(str(x), x.label_) for x in nlp(str(x)).ents])
+    #     for text, label in z.items():
+    #         yield (text,label)
