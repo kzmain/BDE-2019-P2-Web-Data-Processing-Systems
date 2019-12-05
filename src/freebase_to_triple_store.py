3dd36c6653f722199d9f8757ecdcd726b3dc26df
Berk Aydaşgil <berkaydasgil@gmail.com>
2019-12-05 18:45:30 +0100

    triplestore

---
+++ src/freebase_to_triple_store.py
@@ -0,0 +1,27 @@
+# requires this command to be run - export PYTHONPATH=/home/jurbani/trident/build-python
+
+import trident
+
+db = trident.Db('/home/jurbani/data/motherkb-trident')
+
+# Define these variables for a given mention.
+# freebase_label
+# freebase_score
+# freebase_id
+# freebase_entity_type
+
+# put them in a set
+idList.set()
+# idList.add(freebase_id)
+
+
+# facts = {}
+
+def tripleStore():
+    for i in idList:
+        results = db.sparql("SELECT DISTINCT * WHERE { %s ?p ?o .} LIMIT 10"% freebase_label)
+        # TODO:parse trident response
+        # TODO:add the count of facts to the dictionary facts[i]= number_of_facts
+    # TODO: calculate a score based on freebase_score in relation to fact count
+    # TODO: print it.
+    
