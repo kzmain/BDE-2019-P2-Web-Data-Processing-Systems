4bf43b6bed46e8c64e14ff5fdf43f637b4fa5570
Thijmen Kurk <thijmen.j.kurk@gmail.com>
2019-12-20 22:22:31 +0100

    pls

--- setup-and-submit.sh
+++ setup-and-submit.sh
@@ -4,6 +4,7 @@ set -x
 export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
 export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
 export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7
+export PYSPARK_PYTHON="venv/bin/python"
 
 sh setup-env-das.sh
 
@@ -13,7 +14,6 @@ popd
 
 zip -rq src.zip Extractor/ Linking/ NLP/ System/ Tools/
 
-export PYSPARK_PYTHON="venv/bin/python"
 
 spark-submit \
     --name "wdps01-pipeline" \
--- setup-env-das.sh
+++ setup-env-das.sh
@@ -6,13 +6,13 @@ module load python/3.6.0
 module load hadoop/2.7.6
 module load java/jdk-1.8.0
 
+"$PYTHON" -m venv venv
+
+source venv/bin/activate
 export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
 export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
 export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7
-
 export PYSPARK_PYTHON="venv/bin/python"
 
-"$PYTHON" -m venv venv
-source venv/bin/activate
 sh setup-env-local.sh
 deactivate
