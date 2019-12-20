9fde6cd74d80d41ab46ebdc19511e4a0a7d4ab9d
Thijmen Kurk <thijmen.j.kurk@gmail.com>
2019-12-20 18:28:26 +0100

    pls

--- setup-and-submit.sh
+++ setup-and-submit.sh
@@ -8,7 +8,7 @@ module load java/jdk-1.8.0
 
 export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
 export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
-export SPARK_HOME=/home/wdps1901/spark-2.4.4-bin-hadoop2.7
+export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7/
 
 # Set $PYTHON to the Python executable you want to create
 # your virtual environment with. It could just be something
