6434d0f56a4fb44b3bff1a93de046ddce3ff4e6b
Thijmen Kurk <thijmen.j.kurk@gmail.com>
2019-12-20 21:43:52 +0100

    test

--- setup-env.sh
+++ setup-env.sh
@@ -8,7 +8,9 @@ module load java/jdk-1.8.0
 
 export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
 export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
-export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7/
+export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7
+
+export PYSPARK_PYTHON="venv/bin/python"
 
 "$PYTHON" -m venv venv
 source venv/bin/activate
