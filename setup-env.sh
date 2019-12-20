c1d611e8f17bea9b710025b16c28f1e73c88712f
Thijmen Kurk <thijmen.j.kurk@gmail.com>
2019-12-20 21:41:21 +0100

    setup env

---
+++ setup-env.sh
@@ -0,0 +1,18 @@
+set -e
+set -x
+
+module load prun/default
+module load python/3.6.0
+module load hadoop/2.7.6
+module load java/jdk-1.8.0
+
+export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
+export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
+export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7/
+
+"$PYTHON" -m venv venv
+source venv/bin/activate
+pip install -U pip
+pip install -r requirements.pip
+python3 -m spacy download en_core_web_sm
+deactivate
