03abff9613929c69bc30ba83967dff825e0e1703
Thijmen Kurk <thijmen.j.kurk@gmail.com>
2019-12-20 22:27:38 +0100

    activate

---
+++ activate.sh
@@ -0,0 +1,5 @@
+source venv/bin/activate
+export HADOOP_CONF_DIR=/cm/shared/package/hadoop/hadoop-2.7.6/etc/hadoop
+export PYTHON=/cm/shared/package/python/3.6.0/bin/python3
+export SPARK_HOME=/home/bbkruit/spark-2.4.0-bin-hadoop2.7
+export PYSPARK_PYTHON="venv/bin/python"
